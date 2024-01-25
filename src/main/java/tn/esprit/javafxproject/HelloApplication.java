dcb3c6ee3bb4e546b3eaaca08ad11b474199f569
Karim Hadef <karimhadef1996@gmail.com>
2024-01-25 14:19:07 +0100

    ddf

--- src/main/java/tn/esprit/javafxproject/HelloApplication.java
+++ src/main/java/tn/esprit/javafxproject/HelloApplication.java
@@ -4,6 +4,15 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import tn.esprit.javafxproject.utils.DbConnection;
+import tn.esprit.javafxproject.models.Achat;
+import tn.esprit.javafxproject.models.Produit;
+
+import tn.esprit.javafxproject.services.ServiceAchat;
+import tn.esprit.javafxproject.services.ServiceProduit;
+import tn.esprit.javafxproject.utils.DbConnection;
+
+import java.util.ArrayList;
 
 import java.io.IOException;
 
@@ -19,5 +28,37 @@ public class HelloApplication extends Application {
 
     public static void main(String[] args) {
 
+<<<<<<< Updated upstream
+=======
+
+        DbConnection db = DbConnection.getInstance();
+        ServiceAchat SA = new ServiceAchat();
+        ServiceProduit SP = new ServiceProduit();
+        Produit p = new Produit ();
+        p.setProductID(19);
+        SP.add(p);
+
+        //Achat a = new Achat ();
+        //a.setIdAchat(4);
+        //LocalDate dateAchat = LocalDate.now();
+        //a.setDateAchat(dateAchat);
+        //a.setIdClient(2);
+        //SA.update(a);
+        //SA.add(a);
+        //SA.delete(4);
+        ArrayList<Achat> LesAchats = new ArrayList<>();
+        LesAchats = SA.getAll();
+        for (Achat ac : LesAchats) {
+            System.out.println(ac.toString());
+
+        }
+
+        ArrayList<Produit> Lesproduits  = new ArrayList<>();
+        Lesproduits = SP.getAll();
+        for (Produit pc : Lesproduits) {
+            System.out.println(pc.toString());
+
+        }
+>>>>>>> Stashed changes
     }
 }
